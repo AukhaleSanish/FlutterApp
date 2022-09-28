c92644d0544946d87df9dbf027feed71f48e35e6
Sanish Aukhale <sanish.aukhale@srijan.net>
2022-09-28 17:40:43 +0530

    MVVM + null value bug fixed

--- lib/view/productView.dart
+++ lib/view/productView.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
 import '../networking/dataAPI.dart';
 
 class ProdPage extends StatefulWidget {
@@ -15,45 +16,55 @@ class _ProdPageState extends State<ProdPage> {
         appBar: AppBar(
           title: const Text('PRODUCT PAGE'),
         ),
-
         body: Center(
           child: FutureBuilder(
             future: getData(),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
-              return GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: MediaQuery.of(context).size.width * 0.002,
-                ),
-                itemCount: snapshot.data?.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(14),
-                          child: Image.network(snapshot.data[index].image,
-                              width: 120, height: 120),
-                        ),
-                        Container(
-                            margin: const EdgeInsets.all(12),
-                            child: AutoSizeText(
-                              snapshot.data[index].title,
-                              overflow: TextOverflow.ellipsis,maxFontSize: 20,
-                            )),
-                        const SizedBox(
-                          height: 14,
-                        ),
-                        Container(
-                          child: AutoSizeText("\$ " + snapshot.data[index].price,
-                            textAlign: TextAlign.center,maxFontSize: 16,
+              if (snapshot.data == null) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              } else {
+                return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: MediaQuery.of(context).size.width * 0.002,
+                  ),
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(14),
+                            child: Image.network(snapshot.data[index].image,
+                                width: 120, height: 120),
                           ),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              );
+                          Container(
+                              margin: const EdgeInsets.all(12),
+                              child: AutoSizeText(
+                                snapshot.data[index].title,
+                                overflow: TextOverflow.ellipsis,
+                                maxFontSize: 20,
+                              )),
+                          const SizedBox(
+                            height: 14,
+                          ),
+                          Container(
+                            child: AutoSizeText(
+                              "\$ " + snapshot.data[index].price,
+                              textAlign: TextAlign.center,
+                              maxFontSize: 16,
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                );
+              }
             },
           ),
         ));
